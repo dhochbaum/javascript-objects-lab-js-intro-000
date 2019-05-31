04f5a49e35638d83b7143fbfd158d871062168c3
David Hochbaum <dhochbaum+flatiron@gmail.com>
2019-05-31 19:31:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var recipes = new Object();
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  return object
+  a = object
+  a[key] = value
+  return a
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
